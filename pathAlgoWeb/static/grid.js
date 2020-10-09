ce61da1db775f66dc0e8d6326ca91ef3e0815623
Joshua Godinez <jgodi96@gmail.com>
2020-10-08 18:58:47 -0700

    Added wall insert

---
+++ pathAlgoWeb/static/grid.js
@@ -0,0 +1,45 @@
+// function that builds a grid in the "container"
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            $("#container").append("<div class='grid'></div>");
+        };
+    };
+    $(".grid").width(1500/x);
+    $(".grid").height(900/x);
+};
+// function that clears the grid
+function clearGrid(){
+    $(".grid").remove();
+};
+//grid
+$(document).ready(function() {
+    createGrid(20);
+
+    var mouseStillDown = false;
+
+    $(".grid").mousedown(function() {
+      mouseStillDown = true;
+      $(this).css("background-color", "black");
+      doSomething();
+       });
+
+       function doSomething() {
+       if (!mouseStillDown) { return; }
+
+       $(".grid").mouseover(function() {
+       $(this).css("background-color", "black");
+       });
+
+       if (mouseStillDown) {   interval = setTimeout(doSomething,100); }
+   }
+   $(".grid").mouseup(function(event) {
+    mouseStillDown = false;
+});
+
+
+    $(".newGrid").click(function() {
+        refreshGrid();
+
+    });
+});

75412a13403c94a5dcdb5a85bd03e78ad4bf9f79
Joshua Godinez <jgodi96@gmail.com>
2020-10-16 15:47:57 -0700

    Finished Project

--- pathAlgoWeb/static/grid.js
+++ pathAlgoWeb/static/grid.js
@@ -3,27 +3,31 @@ const START_NODE_COL = 3;
 const END_NODE_ROW = 9;
 const END_NODE_COL = 16;
 let wallSet = new Set()
+
+
 //display grid in web
 function displayGrid(gr){
 $("#container").empty()
   const nodes = []
  for (var row =0; row< 20; row++) {
-
    const currentRow = [];
    //rows, columns,
     for (var col= 0; col < 20; col++) {
 
-      if (gr[row][col].row === START_NODE_ROW && gr[row][col].col === START_NODE_COL && gr[row][col].isShortest==false){
+      if (gr[row][col].row === START_NODE_ROW && gr[row][col].col === START_NODE_COL && gr[row][col].isShortest==false && gr[row][col].isWall==false){
         $("#container").append("<div class='cell start-node'></div>");
       }
-      else if (gr[row][col].row === END_NODE_ROW && gr[row][col].col ===END_NODE_COL && gr[row][col].isShortest==false){
+      else if (gr[row][col].row === END_NODE_ROW && gr[row][col].col ===END_NODE_COL && gr[row][col].isShortest==false && gr[row][col].isWall==false){
         $("#container").append("<div class='cell final-node'></div>");
       }
-      else if (gr[row][col].isVisited == true && gr[row][col].isShortest==false){
+      else if (gr[row][col].isVisited == true && gr[row][col].isShortest==false && gr[row][col].isWall==false){
         $("#container").append("<div class='cell visited-node'></div>");
       }
       else if (gr[row][col].isShortest == true){
         $("#container").append("<div class='cell shortest-node'></div>");
+      }
+      else if(gr[row][col].isWall == true){
+        $("#container").append("<div class='cell wall-node'></div>");
       }
       else{
         $("#container").append("<div class='cell unvisit'></div>");
@@ -31,12 +35,13 @@ $("#container").empty()
     }
     console.log("updated grid")
 }
-
 //size of cell
    $(".cell").width(1500/20);
    $(".cell").height(900/20);
 return nodes;
 }
+
+
 // function that builds a grid in the "container"
 function createGrid(size) {
   const nodes = []
@@ -44,7 +49,6 @@ function createGrid(size) {
     const currentRow = [];
     //rows, columns,
      for (var col= 0; col < size; col++) {
-
         const currentNode = {
           col,
           row,
@@ -55,22 +59,21 @@ function createGrid(size) {
           isWall:false,
           previousNode: null,
           isShortest: false,
-
         }
-
          currentRow.push(currentNode);
      }
   nodes.push(currentRow);
-
  }
-
 return nodes;
 };
+
 
 // function that clears the grid
 function refreshGrid(){
     $(".cell").css("background-color",'');
 };
+
+
 //function that contains wall draw logic
 function wallDraw(setWall){
   var mouseStillDown = false;
@@ -84,6 +87,8 @@ function wallDraw(setWall){
     }
     mouseHeldDown();
      });
+
+
 //Function runs when mouse is clicked and held
  function mouseHeldDown() {
        //returns if mouse is not clicked
@@ -97,7 +102,6 @@ function wallDraw(setWall){
         console.log(setWall)
       }
       });
-
      //runs mouseHeldDown function every 100millseconds
      if (mouseStillDown) {   interval = setTimeout(mouseHeldDown,100); }
  }
@@ -109,8 +113,9 @@ function wallDraw(setWall){
   mouseHeldDown();
 });
 return setWall
-  }
-  
+}
+
+
 //animate grid Function
 function animateDijkstra(visitedNodesInOrder,nodesInShortestPath,g){
   console.log(visitedNodesInOrder)
@@ -118,7 +123,6 @@ function animateDijkstra(visitedNodesInOrder,nodesInShortestPath,g){
   const newGrid = g.slice();
   k=0
   for(let i =0; i< visitedNodesInOrder.length; i++){
-
       const nodenode = visitedNodesInOrder[i];
       const newNode = {
         ...nodenode,
@@ -126,21 +130,18 @@ function animateDijkstra(visitedNodesInOrder,nodesInShortestPath,g){
         isShortest: nodesInShortestPath[k].row == visitedNodesInOrder[i].row && nodesInShortestPath[k].col == visitedNodesInOrder[i].col
       };
       newGrid[nodenode.row][nodenode.col] = newNode;
-
       if (nodesInShortestPath[k].row == visitedNodesInOrder[i].row && nodesInShortestPath[k].col == visitedNodesInOrder[i].col){
         if (k!=nodesInShortestPath.length-1)
             k+=1;
         //console.log(k)
       }
-
   }
-
 displayGrid(newGrid);
 console.log(newGrid);
-
 }
+
 //visualize dijstras algorithm Function
-function visualizeDijkstras(g,wallSet) {
+function visualizeDijkstras(g) {
   const startNode = g[START_NODE_ROW][START_NODE_COL]
   const finishNode = g[END_NODE_ROW][END_NODE_COL]
   const visitedNodesInOrder = dijkstra(g,startNode,finishNode)
@@ -148,6 +149,35 @@ function visualizeDijkstras(g,wallSet) {
   //console.log(nodesInShortestPathOrder)
   animateDijkstra(visitedNodesInOrder,nodesInShortestPathOrder,g);
 }
+
+
+//update Nodes function called only when wall
+function updateNodes(set,grids){
+  if (set.size == 0){
+    console.log("set is empty")
+    return grids;
+  }
+  else{
+    k=0
+    for (let i=0; i<grids.length;i++)
+    {
+      for (let j=0;j<grids.length;j++)
+      {
+          if(set.has(k)==true){
+            grids[i][j].isWall=true
+            console.log(grids[i][j].row)
+            console.log(grids[i][j].col)
+            console.log(grids[i][j].isWall)
+          }
+          k+=1
+      }
+    }
+return grids;
+  }
+
+}
+
+
 //main function
 $(document).ready(function() {
 //make grid
@@ -157,26 +187,23 @@ displayGrid(grids);
 //wall draw
     $(".whole-grid").hover(function(){
       wallSet = wallDraw(wallSet);
-      console.log(wallSet);
+      //console.log(wallSet);
+      grids = updateNodes(wallSet,grids)
+      //console.log(grids)
     });
-
 
   //grid
     $(".newGrid").click(function() {
        this.grids = createGrid(20)
        displayGrid(this.grids)
-       console.log(this.grids)
-    });
+       wallSet = new Set()
+       //console.log(this.grids)
+       location.reload();
 
+    });
   //visualize dijstras
   $(".visualize").click(function() {
-
-        visualizeDijkstras(grids,wallSet);
+        visualizeDijkstras(grids);
         console.log("done")
   });
-
-
-
-
-
 });
